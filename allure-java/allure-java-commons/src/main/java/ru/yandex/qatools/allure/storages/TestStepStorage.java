fc8643214b514f17cfd15d5bde644cb177ac9d93
charlie <charlie@yandex-team.ru>
2013-12-10 15:43:07 +0400

    sonar fixes

--- allure-java/allure-java-commons/src/main/java/ru/yandex/qatools/allure/storages/TestStepStorage.java
+++ allure-java/allure-java-commons/src/main/java/ru/yandex/qatools/allure/storages/TestStepStorage.java
@@ -13,34 +13,34 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public final class TestStepStorage {
 
-    private static final Map<Thread, Deque<Step>> testStepData = new ConcurrentHashMap<>();
+    private static final Map<Thread, Deque<Step>> TEST_STEP_DATA = new ConcurrentHashMap<>();
 
     private TestStepStorage() {
     }
 
     private static void checkStep() {
         Thread currentThread = Thread.currentThread();
-        if (!testStepData.containsKey(currentThread)) {
-            testStepData.put(currentThread, new LinkedList<Step>());
+        if (!TEST_STEP_DATA.containsKey(currentThread)) {
+            TEST_STEP_DATA.put(currentThread, new LinkedList<Step>());
         }
 
-        if (testStepData.get(currentThread).isEmpty()) {
-            testStepData.get(currentThread).add(new Step());
+        if (TEST_STEP_DATA.get(currentThread).isEmpty()) {
+            TEST_STEP_DATA.get(currentThread).add(new Step());
         }
     }
 
     public static Step getTestStep() {
         checkStep();
-        return testStepData.get(Thread.currentThread()).getLast();
+        return TEST_STEP_DATA.get(Thread.currentThread()).getLast();
     }
 
     public static void putTestStep(Step testStepResult) {
         checkStep();
-        testStepData.get(Thread.currentThread()).add(testStepResult);
+        TEST_STEP_DATA.get(Thread.currentThread()).add(testStepResult);
     }
 
     public static Step pollTestStep() {
         checkStep();
-        return testStepData.get(Thread.currentThread()).pollLast();
+        return TEST_STEP_DATA.get(Thread.currentThread()).pollLast();
     }
 }
