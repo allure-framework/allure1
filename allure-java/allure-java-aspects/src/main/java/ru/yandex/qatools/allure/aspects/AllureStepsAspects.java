f47bb4e379521d11aa2eeb59523ef334e1dc6451
charlie <charlie@yandex-team.ru>
2013-12-13 19:01:07 +0400

    temporary aspect changes

--- allure-java/allure-java-aspects/src/main/java/ru/yandex/qatools/allure/aspects/AllureStepsAspects.java
+++ allure-java/allure-java-aspects/src/main/java/ru/yandex/qatools/allure/aspects/AllureStepsAspects.java
@@ -6,8 +6,8 @@ import org.aspectj.lang.reflect.MethodSignature;
 import ru.yandex.qatools.allure.Allure;
 import ru.yandex.qatools.allure.annotations.Step;
 import ru.yandex.qatools.allure.events.StepFailureEvent;
-import ru.yandex.qatools.allure.events.StepStartEvent;
-import ru.yandex.qatools.allure.events.StepStopEvent;
+import ru.yandex.qatools.allure.events.StepFinishedEvent;
+import ru.yandex.qatools.allure.events.StepStartedEvent;
 
 /**
  * @author Dmitry Baev charlie@yandex-team.ru
@@ -17,32 +17,32 @@ import ru.yandex.qatools.allure.events.StepStopEvent;
 @Aspect
 public class AllureStepsAspects {
 
-	@Pointcut("@annotation(ru.yandex.qatools.allure.annotations.Step)")
-	public void withStepAnnotation() {
+    @Pointcut("@annotation(ru.yandex.qatools.allure.annotations.Step)")
+    public void withStepAnnotation() {
         //pointcut body, should be empty
-	}
-
-	@Pointcut("execution(* *(..))")
-	public void anyMethod() {
-        //pointcut body, should be empty
-	}
-
-	@Before("anyMethod() && withStepAnnotation()")
-	public void stepStart(JoinPoint joinPoint) {
-		MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
-		Step step = methodSignature.getMethod().getAnnotation(Step.class);
-		String stepTitle = AllureAspectUtils.getTitle(step.value(), methodSignature.getName(), joinPoint.getArgs());
-        Allure.LIFECYCLE.fire(new StepStartEvent(stepTitle));
     }
 
-	@AfterThrowing(pointcut = "anyMethod() && withStepAnnotation()", throwing = "e")
-	public void stepFailed(JoinPoint joinPoint, Throwable e) {
-        Allure.LIFECYCLE.fire(new StepFailureEvent(e));
-        Allure.LIFECYCLE.fire(new StepStopEvent());
-	}
+    @Pointcut("execution(* *(..))")
+    public void anyMethod() {
+        //pointcut body, should be empty
+    }
 
-	@AfterReturning(pointcut = "anyMethod() && withStepAnnotation()", returning = "result")
-	public void stepStop(JoinPoint joinPoint, Object result) {
-        Allure.LIFECYCLE.fire(new StepStopEvent());
+    @Before("anyMethod() && withStepAnnotation()")
+    public void stepStart(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Step step = methodSignature.getMethod().getAnnotation(Step.class);
+        String stepTitle = AllureAspectUtils.getTitle(step.value(), methodSignature.getName(), joinPoint.getArgs());
+        Allure.LIFECYCLE.fire(new StepStartedEvent().withTitle(stepTitle));
+    }
+
+    @AfterThrowing(pointcut = "anyMethod() && withStepAnnotation()", throwing = "e")
+    public void stepFailed(JoinPoint joinPoint, Throwable e) {
+        Allure.LIFECYCLE.fire(new StepFailureEvent().withThrowable(e));
+        Allure.LIFECYCLE.fire(new StepFinishedEvent());
+    }
+
+    @AfterReturning(pointcut = "anyMethod() && withStepAnnotation()", returning = "result")
+    public void stepStop(JoinPoint joinPoint, Object result) {
+        Allure.LIFECYCLE.fire(new StepFinishedEvent());
     }
 }
