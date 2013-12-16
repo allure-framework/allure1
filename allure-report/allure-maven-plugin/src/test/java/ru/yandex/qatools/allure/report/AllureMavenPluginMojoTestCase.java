75f49af1ae0d91d0a0b20397daf93ef5eef323aa
charlie <charlie@yandex-team.ru>
2013-12-16 14:43:06 +0400

    maven-plugin-testing: dependency hell resolved!!!

--- allure-report/allure-maven-plugin/src/test/java/ru/yandex/qatools/allure/report/AllureMavenPluginMojoTestCase.java
+++ allure-report/allure-maven-plugin/src/test/java/ru/yandex/qatools/allure/report/AllureMavenPluginMojoTestCase.java
@@ -1,6 +1,11 @@
 package ru.yandex.qatools.allure.report;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+import java.io.File;
+import java.io.InputStream;
 
 /**
  * @author Dmitry Baev charlie@yandex-team.ru
@@ -8,6 +13,18 @@ import org.apache.maven.plugin.testing.AbstractMojoTestCase;
  */
 public class AllureMavenPluginMojoTestCase extends AbstractMojoTestCase {
 
+    static {
+        //TODO dirty hack??
+//        System.setProperty("basedir", new File("allure-report/allure-maven-plugin").getAbsolutePath());
+    }
+
     public void testSimple() throws Exception {
+
+//        File pluginConfig = new File(getBasedir(), "src/test/resources/unit/plugin-config.xml");
+//        System.out.println(pluginConfig.exists());
+//        File pluginConfig = FileUtils.toFile(getClassLoader().getResource("unit/plugin-config.xml"));
+//        AllureMavenPlugin mojo = (AllureMavenPlugin)super.lookupMojo("allure-maven-plugin", pluginConfig);
+
+//        assertNotNull(mojo);
     }
 }
