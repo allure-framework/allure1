4c45a013efcbe5ca9218835b162642dd50ed1e5e
just-boris <just-boris@yandex-team.ru>
2013-12-18 15:42:44 +0400

    handle zero values in duration histogram

--- allure-report/allure-report-face/src/test/webapp/unit/charts/durationSpec.js
+++ allure-report/allure-report-face/src/test/webapp/unit/charts/durationSpec.js
@@ -10,24 +10,29 @@ describe('SeverityMap', function () {
     }));
     jasmine.qatools.mockD3Tooltip();
 
+    var defaultData = {data: [
+        {status: 'PASSED', time: {duration: 2}},
+        {status: 'PASSED', time: {duration: 45}},
+        {status: 'FAILED', time: {duration: 34}},
+        {status: 'PASSED', time: {duration: 42}},
+        {status: 'PASSED', time: {duration: 31}},
+        {status: 'PASSED', time: {duration: 4}}
+    ]};
+
+    function extractData(index, elem) {
+        return elem.__data__.testcases.length;
+    }
+
     function createElement(html, scopeValues) {
         inject(function ($compile, $rootScope) {
             scope = $rootScope.$new();
-            angular.extend(scope, scopeValues);
+            angular.extend(scope, scopeValues || defaultData);
             elem = angular.element(html).appendTo('body');
             $compile(elem)(scope);
             scope.$digest();
         });
     }
     beforeEach(function() {
-        createElement('<div duration data="data"></div>', {data: [
-            {status: 'PASSED', time: {duration: 2}},
-            {status: 'PASSED', time: {duration: 45}},
-            {status: 'FAILED', time: {duration: 34}},
-            {status: 'PASSED', time: {duration: 42}},
-            {status: 'PASSED', time: {duration: 31}},
-            {status: 'PASSED', time: {duration: 4}}
-        ]});
         d3.timer.flush();
     });
     afterEach(function() {
@@ -35,13 +40,21 @@ describe('SeverityMap', function () {
     });
 
     it('should create graph and distribute testcases in intervals', function() {
-        expect(elem.find('.bar').map(function() {
-            return this.__data__.testcases.length;
-        }).toArray()).toEqual([2, 0, 0, 0, 0, 0, 2, 0, 2]);
+        createElement('<div duration data="data"></div>');
+        expect(elem.find('.bar').map(extractData).toArray()).toEqual([2, 0, 0, 0, 0, 0, 2, 0, 2]);
     });
 
     it('should format ticks using time filter', function() {
+        createElement('<div duration data="data"></div>');
         expect(filterSpy).toHaveBeenCalled();
+    });
+
+    it('should create diagram only with zero durations', function() {
+        createElement('<div duration data="data"></div>', { data: [
+            {status: 'SKIPPED', time: {duration:0}},
+            {status: 'SKIPPED', time: {duration:0}}
+        ]});
+        expect(elem.find('.bar').map(extractData).toArray()).toEqual([2, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     });
 
     afterEach(function() {
