2a4ce797523d4a63546d1aae4d9bb23191901a34
Artem Eroshenko <eroshenkoam@yandex-team.ru>
2013-12-18 19:53:45 +0400

    Add generation of attach event

--- allure-java/allure-java-commons/src/main/java/ru/yandex/qatools/allure/events/MakeAttachEvent.java
+++ allure-java/allure-java-commons/src/main/java/ru/yandex/qatools/allure/events/MakeAttachEvent.java
@@ -10,15 +10,12 @@ import static ru.yandex.qatools.allure.utils.AllureWriteUtils.writeAttachment;
  * @author Dmitry Baev charlie@yandex-team.ru
  *         Date: 11.11.13
  */
-public class MakeAttachEvent implements StepEvent {
-    private String title;
-    private AttachmentType attachmentType;
-    private Object attach;
+public class MakeAttachEvent extends AbstractMakeAttachEvent {
 
     public MakeAttachEvent(String title, AttachmentType attachmentType, Object attach) {
-        this.title = title;
-        this.attachmentType = attachmentType;
-        this.attach = attach;
+        setTitle(title);
+        setAttachmentType(attachmentType);
+        setAttach(attach);
     }
 
     @Override
@@ -26,40 +23,16 @@ public class MakeAttachEvent implements StepEvent {
         Attachment attachment = new Attachment();
 
         String source = writeAttachment(
-                attach,
-                attachmentType,
+                getAttach(),
+                getAttachmentType(),
                 ".attach"
         );
 
-        attachment.setTitle(title);
-        attachment.setType(attachmentType);
+        attachment.setTitle(getTitle());
+        attachment.setType(getAttachmentType());
         attachment.setSource(source);
 
         step.getAttachments().add(attachment);
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public AttachmentType getAttachmentType() {
-        return attachmentType;
-    }
-
-    public void setAttachmentType(AttachmentType attachmentType) {
-        this.attachmentType = attachmentType;
-    }
-
-    public Object getAttach() {
-        return attach;
-    }
-
-    public void setAttach(Object attach) {
-        this.attach = attach;
     }
 
 }
