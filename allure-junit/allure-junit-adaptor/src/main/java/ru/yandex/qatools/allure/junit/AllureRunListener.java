dffe624de05664a7ded09fb6880bd5460bd7482a
charlie <charlie@yandex-team.ru>
2014-01-16 20:10:42 +0400

    added allure run listener test

--- allure-junit/allure-junit-adaptor/src/main/java/ru/yandex/qatools/allure/junit/AllureRunListener.java
+++ allure-junit/allure-junit-adaptor/src/main/java/ru/yandex/qatools/allure/junit/AllureRunListener.java
@@ -20,11 +20,10 @@ public class AllureRunListener extends RunListener {
 
     private Allure lifecycle = Allure.LIFECYCLE;
 
-    private static final Map<String, String> SUITES = new HashMap<>();
+    private final Map<String, String> suites = new HashMap<>();
 
     @Override
     public void testStarted(Description description) {
-        System.out.println("testStarted");
         String suiteUid = getSuiteUid(description);
 
         TestCaseStartedEvent event = new TestCaseStartedEvent(suiteUid, description.getMethodName());
@@ -32,74 +31,68 @@ public class AllureRunListener extends RunListener {
 
         am.update(event);
 
-        Allure.LIFECYCLE.fire(event);
+        getLifecycle().fire(event);
     }
 
     @Override
     public void testFinished(Description description) {
-        System.out.println("testFinished");
-        Allure.LIFECYCLE.fire(new TestCaseFinishedEvent());
+        getLifecycle().fire(new TestCaseFinishedEvent());
     }
 
     @Override
     public void testFailure(Failure failure) {
-        System.out.println("testFailure");
-        Allure.LIFECYCLE.fire(new TestCaseFailureEvent().withThrowable(failure.getException()));
+        getLifecycle().fire(new TestCaseFailureEvent().withThrowable(failure.getException()));
     }
 
     @Override
     public void testAssumptionFailure(Failure failure) {
-        System.out.println("testAssumptionFailure");
-        Allure.LIFECYCLE.fire(new TestCaseSkippedEvent().withThrowable(failure.getException()));
+        getLifecycle().fire(new TestCaseSkippedEvent().withThrowable(failure.getException()));
     }
 
     @Override
     public void testIgnored(Description description) {
-        System.out.println("testIgnored");
-        //if test class annotated with @Ignored
+        //if test class annotated with @Ignored do nothing
         if (description.getMethodName() == null) {
             return;
         }
 
         testStarted(description);
-        Allure.LIFECYCLE.fire(new TestCaseSkippedEvent().withThrowable(new Exception("Test ignored.")));
+        getLifecycle().fire(new TestCaseSkippedEvent().withThrowable(new Exception("Test ignored.")));
         testFinished(description);
     }
 
     @Override
     public void testRunFinished(Result result) {
-        System.out.println("testRunFinished");
-        for (String uid : SUITES.values()) {
+        for (String uid : getSuites().values()) {
             testSuiteFinished(uid);
         }
     }
 
     public void testSuiteStarted(String uid, String suiteName, Collection<Annotation> annotations) {
-        System.out.println("testSuiteStarted");
         TestSuiteStartedEvent event = new TestSuiteStartedEvent(uid, suiteName);
         AnnotationManager am = new AnnotationManager(annotations);
 
         am.update(event);
 
-        Allure.LIFECYCLE.fire(event);
+        getLifecycle().fire(event);
     }
 
     public void testSuiteFinished(String uid) {
-        System.out.println("testSuiteFinished");
-        Allure.LIFECYCLE.fire(new TestSuiteFinishedEvent(uid));
+        getLifecycle().fire(new TestSuiteFinishedEvent(uid));
     }
 
 
-    private String getSuiteUid(Description description) {
+    public String getSuiteUid(Description description) {
         String suiteName = description.getClassName();
-        Collection<Annotation> annotations = Arrays.asList(description.getTestClass().getAnnotations());
-        if (!SUITES.containsKey(suiteName)) {
+        if (!getSuites().containsKey(suiteName)) {
             String uid = UUID.randomUUID().toString();
+
+            Collection<Annotation> annotations = Arrays.asList(description.getTestClass().getAnnotations());
             testSuiteStarted(uid, suiteName, annotations);
-            SUITES.put(suiteName, uid);
-            return uid;
+
+            getSuites().put(suiteName, uid);
         }
-        return SUITES.get(suiteName);
+        return getSuites().get(suiteName);
     }
 
     public Allure getLifecycle() {
@@ -108,5 +101,9 @@ public class AllureRunListener extends RunListener {
 
     public void setLifecycle(Allure lifecycle) {
         this.lifecycle = lifecycle;
+    }
+
+    public Map<String, String> getSuites() {
+        return suites;
     }
 }
---
+++ allure-junit/allure-junit-adaptor/src/test/java/ru/yandex/qatools/allure/junit/testdata/TestData.java
@@ -0,0 +1,8 @@
+package ru.yandex.qatools.allure.junit.testdata;
+
+/**
+ * @author Dmitry Baev charlie@yandex-team.ru
+ *         Date: 16.01.14
+ */
+public class TestData {
+}
