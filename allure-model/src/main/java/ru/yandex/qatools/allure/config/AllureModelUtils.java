e889ad1ce648571246b463b48d2a892d5904e230
charlie <charlie@yandex-team.ru>
2014-01-20 14:04:16 +0400

    refactoring model utils

--- allure-model/src/main/java/ru/yandex/qatools/allure/config/AllureModelUtils.java
+++ allure-model/src/main/java/ru/yandex/qatools/allure/config/AllureModelUtils.java
@@ -3,10 +3,11 @@ package ru.yandex.qatools.allure.config;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
 import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
-import java.io.File;
+import java.io.InputStream;
 
 /**
  * @author Dmitry Baev charlie@yandex-team.ru
@@ -18,10 +19,10 @@ public final class AllureModelUtils {
 
     public static Validator getAllureSchemaValidator() throws SAXException {
         String schemaFileName = AllureModelConfig.newInstance().getSchemaFileName();
-        File schemaFile = new File(ClassLoader.getSystemResource(schemaFileName).getFile());
+        InputStream schemaFile = ClassLoader.getSystemResourceAsStream(schemaFileName);
         SchemaFactory schemaFactory = SchemaFactory
                 .newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-        Schema schema = schemaFactory.newSchema(schemaFile);
+        Schema schema = schemaFactory.newSchema(new StreamSource(schemaFile));
         return schema.newValidator();
     }
 }
