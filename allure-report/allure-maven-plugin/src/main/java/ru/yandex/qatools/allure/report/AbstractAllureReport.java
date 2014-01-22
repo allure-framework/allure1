ca2860dd221061c79f12ce6a329a860762cdf437
Artem Eroshenko <eroshenkoam@yandex-team.ru>
2014-01-22 19:31:37 +0400

    Refactoring of allure maven plugin

--- allure-report/allure-maven-plugin/src/main/java/ru/yandex/qatools/allure/report/AbstractAllureReport.java
+++ allure-report/allure-maven-plugin/src/main/java/ru/yandex/qatools/allure/report/AbstractAllureReport.java
@@ -1,16 +1,103 @@
 package ru.yandex.qatools.allure.report;
 
-import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.doxia.siterenderer.Renderer;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.BuildPluginManager;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.reporting.AbstractMavenReport;
+import ru.yandex.qatools.allure.config.AllureResultsConfig;
+
+import java.io.File;
+import java.util.Locale;
 
 /**
  * @author Artem Eroshenko eroshenkoam@yandex-team.ru
  *         Date: 1/20/14
  */
-public abstract class AbstractAllureReport extends AbstractMojo {
+public abstract class AbstractAllureReport extends AbstractMavenReport {
+
+    @Parameter(defaultValue = "${project}", required = true, readonly = true)
+    protected MavenProject project;
 
     @SuppressWarnings("unused")
-    @Parameter(property = "allure.report.directory", defaultValue = "${project.build.directory}/site/")
+    @Parameter(property = "allure.report.directory", defaultValue = "${project.reporting.outputDirectory}")
     private String outputDirectory;
+
+    @Parameter(alias = "allure.results.directory", required = false)
+    private File allureResultsDirectory = AllureResultsConfig.newInstance().getResultsDirectory();
+
+    @Parameter
+    private Dependency reportFace = new Dependency(
+            "ru.yandex.qatools.allure",
+            "allure-report-face",
+            "${allure.version}",
+            "war"
+    );
+
+    @Parameter(defaultValue = "${session}", required = true, readonly = true)
+    protected MavenSession mavenSession;
+
+    @Component
+    protected BuildPluginManager pluginManager;
+
+    @Component
+    protected PluginDescriptor plugin;
+
+    @Component
+    protected Renderer siteRenderer;
+
+
+    @Override
+    protected final MavenProject getProject() {
+        return project;
+    }
+
+    @Override
+    protected final Renderer getSiteRenderer() {
+        return siteRenderer;
+    }
+
+    @Override
+    public String getName(Locale locale) {
+        return "Allure";
+    }
+
+    @Override
+    public String getOutputName() {
+        return plugin.getArtifactId();
+    }
+
+    @Override
+    public final String getOutputDirectory() {
+        return outputDirectory;
+    }
+
+    protected final Dependency getReportFace() {
+        return reportFace;
+    }
+
+    protected final MavenSession getMavenSession() {
+        return mavenSession;
+    }
+
+    protected final BuildPluginManager getPluginManager() {
+        return pluginManager;
+    }
+
+    public final File getAllureReportDirectory() {
+        return new File(getReportOutputDirectory(), plugin.getArtifactId());
+    }
+
+    public File getAllureResultsDirectory() {
+        return allureResultsDirectory;
+    }
+
+    @Override
+    public String getDescription(Locale locale) {
+        return "Extended report on the test results of the project.";
+    }
 
 }
--- allure-report/allure-maven-plugin/src/main/java/ru/yandex/qatools/allure/report/AllureReport.java
+++ allure-report/allure-maven-plugin/src/main/java/ru/yandex/qatools/allure/report/AllureReport.java
@@ -1,34 +1,46 @@
 package ru.yandex.qatools.allure.report;
 
 import org.apache.maven.doxia.sink.Sink;
-import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.reporting.MavenReportException;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import ru.yandex.qatools.allure.data.AllureReportGenerator;
 
+import java.io.File;
 import java.util.Locale;
+
+import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.configuration;
+import static org.twdata.maven.mojoexecutor.MojoExecutor.element;
 
 /**
  * @author Artem Eroshenko eroshenkoam@yandex-team.ru
  *         Date: 1/20/14
  */
-@Mojo(name = "report")
+@SuppressWarnings("unused")
+@Mojo(name = "report", defaultPhase = LifecyclePhase.SITE)
 public class AllureReport extends AbstractAllureReport {
 
-    /**
-     * {@inheritDoc}
-     */
-    public String getOutputName() {
-        return plugin.getArtifactId();
-    }
 
     @Override
     protected void executeReport(Locale locale) throws MavenReportException {
-        Sink sink = getSink();
+        File allureReportDirectory = getAllureReportDirectory();
+        File allureResultsDirectory = getAllureResultsDirectory();
 
+        AllureReportGenerator generator = new AllureReportGenerator(allureResultsDirectory);
+        generator.generate(allureReportDirectory);
+
+        unpackAllureReportFace();
+
+        render(getSink(), getName(locale));
+    }
+
+    private void render(Sink sink, String title) {
         sink.head();
         sink.title();
-        sink.text(getName(locale));
+        sink.text(title);
         sink.title_();
         sink.head_();
         sink.body();
@@ -42,4 +54,36 @@ public class AllureReport extends AbstractAllureReport {
         sink.flush();
         sink.close();
     }
+
+    private void unpackAllureReportFace() throws MavenReportException {
+        try {
+            executeMojo(
+                    plugin("org.apache.maven.plugins", "maven-dependency-plugin", "2.8"),
+                    goal("unpack"),
+                    getDependencyPluginConfiguration(),
+
+                    executionEnvironment(getProject(), getMavenSession(), getPluginManager())
+            );
+
+        } catch (MojoExecutionException e) {
+            throw new MavenReportException(e.getMessage());
+        }
+    }
+
+    private Xpp3Dom getDependencyPluginConfiguration() {
+        return configuration(
+                element(
+                        "artifactItems",
+                        element("artifactItem",
+                                element("groupId", getReportFace().getGroupId()),
+                                element("artifactId", getReportFace().getArtifactId()),
+                                element("version", getReportFace().getVersion()),
+                                element("overWrite", "true"),
+                                element("outputDirectory", getAllureReportDirectory().getAbsolutePath()),
+                                element("type", getReportFace().getPackaging())
+                        )
+                )
+        );
+    }
+
 }
