2728785d1a46252c465e545ea4f4ede669e9d9f4
charlie <charlie@yandex-team.ru>
2014-05-05 17:41:23 +0400

    add remove attachment event test
    add remove attachment event test

---
+++ allure-java/allure-java-commons/src/test/java/ru/yandex/qatools/allure/events/RemoveAttachmentsEventTest.java
@@ -0,0 +1,88 @@
+package ru.yandex.qatools.allure.events;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import ru.yandex.qatools.allure.model.Attachment;
+import ru.yandex.qatools.allure.model.AttachmentType;
+import ru.yandex.qatools.allure.model.Step;
+import ru.yandex.qatools.allure.utils.AllureResultsUtils;
+
+import java.io.File;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static ru.yandex.qatools.allure.utils.AllureResultsUtils.writeAttachment;
+import static ru.yandex.qatools.allure.utils.DirectoryMatcher.contains;
+import static ru.yandex.qatools.allure.utils.DirectoryMatcher.notContains;
+
+/**
+ * @author Dmitry Baev charlie@yandex-team.ru
+ *         Date: 05.05.14
+ */
+public class RemoveAttachmentsEventTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    private Step root = new Step();
+
+    private File resultsDirectory;
+
+    @Before
+    public void setUp() throws Exception {
+        resultsDirectory = folder.newFolder();
+        AllureResultsUtils.setResultsDirectory(resultsDirectory);
+
+        Step child1 = new Step().withName("child1");
+        Step child2 = new Step().withName("child2");
+
+        child1.getAttachments().add(save("firstAttach"));
+        child2.getAttachments().add(save("secondAttach"));
+
+        root.getAttachments().add(save("rootAttach"));
+        root.getSteps().add(child1);
+        root.getSteps().add(child2);
+    }
+
+    @Test
+    public void removeAttachmentsByDefaultTest() throws Exception {
+        String[] files = resultsDirectory.list();
+
+        new RemoveAttachmentsEvent("a^").process(root);
+
+        for (String file : files) {
+            assertThat(resultsDirectory, contains(file));
+        }
+
+        checkAttachmentsCount(root, 1);
+    }
+
+    @Test
+    public void removeAllAttachmentsTest() throws Exception {
+        String[] files = resultsDirectory.list();
+
+        new RemoveAttachmentsEvent(".*").process(root);
+
+        for (String file : files) {
+            assertThat(resultsDirectory, notContains(file));
+        }
+
+        checkAttachmentsCount(root, 0);
+    }
+
+    public void checkAttachmentsCount(Step root, int count) {
+        assertThat(count, is(root.getAttachments().size()));
+        for (Step step : root.getSteps()) {
+            checkAttachmentsCount(step, count);
+        }
+    }
+
+    public Attachment save(String string) {
+        String source = writeAttachment(string, AttachmentType.TXT);
+
+        return new Attachment().withSource(source).withType(AttachmentType.TXT).withTitle("other-title");
+    }
+
+}
