d382cf7eab9c35e8c2d86faf5ccf9e082e120ef2
charlie <charlie@yandex-team.ru>
2014-05-05 15:02:17 +0400

    Add ability to remove attachments when test finished

--- allure-model/src/main/java/ru/yandex/qatools/allure/config/AllureReportConfig.java
+++ allure-model/src/main/java/ru/yandex/qatools/allure/config/AllureReportConfig.java
@@ -1,8 +1,33 @@
 package ru.yandex.qatools.allure.config;
 
+import ru.yandex.qatools.properties.PropertyLoader;
+import ru.yandex.qatools.properties.annotations.Property;
+import ru.yandex.qatools.properties.annotations.Resource;
+
 /**
  * @author Artem Eroshenko eroshenkoam@yandex-team.ru
  *         Date: 12/13/13
  */
+@SuppressWarnings("unused")
+@Resource.Classpath("allure.report.properties")
 public class AllureReportConfig {
+
+    /**
+     * Regex to remove attachments in passed test.
+     * Will be removed in 1.4.0
+     */
+    @Property("allure.report.remove.attachments")
+    private String removeAttachments = "a^";
+
+    public AllureReportConfig() {
+        PropertyLoader.populate(this);
+    }
+
+    public String getRemoveAttachments() {
+        return removeAttachments;
+    }
+
+    public static AllureReportConfig newInstance() {
+        return new AllureReportConfig();
+    }
 }
