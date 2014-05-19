2eb1c2ccb4f115515d1975c7636537725c96db74
just-boris <just-boris@yandex-team.ru>
2014-05-19 19:00:37 +0400

    make e2e tests passed

--- allure-e2e/src/test/java/ru/yandex/qatools/allure/e2e/tabs/XUnitTabTest.java
+++ allure-e2e/src/test/java/ru/yandex/qatools/allure/e2e/tabs/XUnitTabTest.java
@@ -1,23 +1,15 @@
 package ru.yandex.qatools.allure.e2e.tabs;
 
-import com.google.common.base.Joiner;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import ru.yandex.qatools.allure.JSErrorsRule;
 import ru.yandex.qatools.allure.Page;
-
-import java.text.MessageFormat;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElement;
 import static ru.yandex.qatools.allure.Helpers.existsAndVisible;
 import static ru.yandex.qatools.htmlelements.matchers.WrapsElementMatchers.exists;
 import static ru.yandex.qatools.matchers.decorators.MatcherDecorators.should;
@@ -31,8 +23,6 @@ public class XUnitTabTest {
     public JSErrorsRule jsErrorsRule = new JSErrorsRule();
 
     private Page page;
-
-    WebDriver driver;
 
     @Before
     public void openBrowser() throws Exception {
