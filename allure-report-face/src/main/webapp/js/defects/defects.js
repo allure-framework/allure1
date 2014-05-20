5a5c22f7636cfa8cb4e13dfba9f4a0ed7d83d060
just-boris <just-boris@yandex-team.ru>
2014-05-20 19:10:11 +0400

    special page for defect view

--- allure-report-face/src/main/webapp/js/defects/defects.js
+++ allure-report-face/src/main/webapp/js/defects/defects.js
@@ -1,29 +1,55 @@
 /*global angular*/
-angular.module('allure.defects', []).controller('DefectsCtrl', function($scope, $state, orderByFilter, status, Collection, defects) {
+angular.module('allure.defects', []).controller('DefectsCtrl', function($scope, $state, orderByFilter, status, WatchingStore, Collection, defects) {
     "use strict";
+    var store = new WatchingStore('defectsSettings');
     $scope.isState = function(statename) {
         return $state.is(statename);
     };
-    $scope.setTestcase = function(testcase) {
-        $state.go('defects.testcase', {testcaseUid: testcase.uid});
+    $scope.setDefect = function(defect) {
+        //TODO @charlie: here should be:
+        //$state.go('defects.defect', {defectUid: defect.uid});
+        $state.go('defects.defect', {defectUid: defect.failure.message});
     };
     $scope.select = function(direction) {
-        var index = testcases.indexOf($scope.testcase),
-            testcase = direction < 0 ? testcases.getPrevious(index) : testcases.getNext(index);
-        $scope.setTestcase(testcase);
+        var index = defectList.indexOf($scope.defect),
+            defect = direction < 0 ? defectList.getPrevious(index) : defectList.getNext(index);
+        $scope.setDefect(defect);
     };
     $scope.defects = orderByFilter(defects.defectsList, function(defectType) {
         return status.getSortOrder(defectType.status);
     });
-    var testcases = new Collection($scope.defects.reduce(function(testcases, type) {
-        return type.defects.reduce(function(testcases, defect) {
-            return testcases.concat(defect.testCases);
-        }, testcases);
+    $scope.showStatuses = {};
+    $scope.testcase = {};
+    $scope.sorting = {
+        predicate: store.bindProperty($scope, 'sorting.predicate', 'statistic.failed'),
+        reverse: store.bindProperty($scope, 'sorting.reverse', false)
+    };
+    status.all.forEach(function(status) {
+        $scope.showStatuses[status] = true;
+    });
+    var defectList = new Collection($scope.defects.reduce(function(defects, type) {
+        type.defects.forEach(function(defect) {
+            defect.type = type.status;
+        });
+        return defects.concat(type.defects);
     }, []));
+    $scope.$watch('testcase.uid', function(testcaseUid, oldUid) {
+        if(testcaseUid && testcaseUid !== oldUid) {
+            $state.go('defects.defect.testcase', {testcaseUid: testcaseUid});
+        }
+    });
     $scope.$on('$stateChangeSuccess', function(event, state, params) {
-        delete $scope.testcase;
+        delete $scope.testcase.uid;
+        delete $scope.defect;
+        if(params.defectUid) {
+            //TODO @charlie: here should be:
+            //$scope.defect = defectList.findBy('uid', params.defectUid);
+            $scope.defect = defectList.items.filter(function(defect) {
+                return defect.failure.message === params.defectUid;
+            })[0];
+        }
         if(params.testcaseUid) {
-            $scope.testcase = testcases.findBy('uid', params.testcaseUid);
+            $scope.testcase.uid = params.testcaseUid;
         }
     });
 });
--- allure-report-face/src/test/webapp/unit/defects/defectsSpec.js
+++ allure-report-face/src/test/webapp/unit/defects/defectsSpec.js
@@ -3,16 +3,21 @@ describe('DefectsCtrl', function () {
     "use strict";
     var scope;
 
-    function DefectType(title, defects) {
-
-    }
-    function Defect(message, testcases) {
+    function Defect(uid, message, testcases) {
+        this.uid = uid;
         this.failure = {message: message};
         this.testCases = testcases;
     }
 
     beforeEach(module('allure.defects'));
-    beforeEach(module('allure.services'));
+    beforeEach(function() {
+        var self = this;
+        module('allure.services', function($provide) {
+            $provide.value('WatchingStore', function() {
+                return self.WatchingStore = jasmine.createSpyObj('WatchingStore', ['bindProperty']);
+            });
+        });
+    });
     beforeEach(inject(function ($controller, $rootScope) {
         this.$state = jasmine.createSpyObj('$state', ['go', 'is']);
         this.createController = function(defects) {
@@ -30,15 +35,15 @@ describe('DefectsCtrl', function () {
         scope =this.createController([{
             title:'Test', status: 'BROKEN',
             defects: [
-                new Defect('FileNotFoundException: No file found', [{uid: 4}]),
-                new Defect('ArrayIndexOutOfBoundsException: 100500', [{uid: 5}])
+                new Defect(1, 'FileNotFoundException: No file found', [{uid: 4}]),
+                new Defect(2, 'ArrayIndexOutOfBoundsException: 100500', [{uid: 5}])
             ]
         }, {
             title:'Product', status: 'FAILED',
             defects: [
-                new Defect('Assertion Error: expected 3 to be 4', [{uid: 1}]),
-                new Defect('Assertion Error: expected true to be false', [{uid: 2}]),
-                new Defect('Assertion Error: expected value to be defined', [{uid: 3}])
+                new Defect(3, 'Assertion Error: expected 3 to be 4', [{uid: 1}]),
+                new Defect(4, 'Assertion Error: expected true to be false', [{uid: 2}]),
+                new Defect(5, 'Assertion Error: expected value to be defined', [{uid: 3}])
             ]
         }]);
     });
@@ -48,9 +53,17 @@ describe('DefectsCtrl', function () {
         expect(scope.defects[1].title).toBe('Test');
     });
 
-    it('should not select testcase by default', function() {
+    it('should not select testcase an defect by default', function() {
         scope.$broadcast('$stateChangeSuccess', null, {});
-        expect(scope.testcase).toBeUndefined();
+        expect(scope.testcase.uid).toBeUndefined();
+        expect(scope.defect).toBeUndefined();
+    });
+
+    it('should find and select defect by uid', function() {
+        //TODO @charlie: here should be:
+        //scope.$broadcast('$stateChangeSuccess', null, {defectUid: 3});
+        scope.$broadcast('$stateChangeSuccess', null, {defectUid: 'Assertion Error: expected 3 to be 4'});
+        expect(scope.defect.uid).toBe(3);
     });
 
     it('should find and select testcase by uid', function() {
