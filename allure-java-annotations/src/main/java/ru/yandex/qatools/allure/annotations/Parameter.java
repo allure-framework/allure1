9de548731cde8e03557317f9c3fed84b5bc44457
Dmitry Baev <baev.dm@gmail.com>
2014-08-27 16:07:58 +0400

    Add javadoc for `@Parameter` annotation

--- allure-java-annotations/src/main/java/ru/yandex/qatools/allure/annotations/Parameter.java
+++ allure-java-annotations/src/main/java/ru/yandex/qatools/allure/annotations/Parameter.java
@@ -9,6 +9,28 @@ import java.lang.annotation.Target;
 /**
  * @author Dmitry Baev charlie@yandex-team.ru
  *         Date: 19.06.14
+ *
+ *         You can use this annotation to add parameters to your tests:
+ *
+ *         {@code
+ *         @Parameter("My Param")
+ *         private String myParameter;
+ *
+ *         @Test
+ *         public void suite2Test() throws Exception {
+ *              myParameter = "first";
+ *              myParameter = "second";
+ *              myParameter = "third";
+ *         }
+ *         }
+ *
+ *         All three values will be added to report
+ *
+ *         Note that the initializations of constant fields (static final fields
+ *         where the initializer is a constant string object or primitive value)
+ *         are not join points, since Java requires their references to be inlined.
+ *
+ *         value - it's name of paramter, field name by default
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
