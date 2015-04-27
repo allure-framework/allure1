558af8863abd5e190982b65366a6f66c8685eab1
Dmitry Baev <charlie@yandex-team.ru>
2015-04-27 18:34:41 +0300

    remove unused functions from controler

--- allure-report-face/src/main/webapp/js/controllers.js
+++ allure-report-face/src/main/webapp/js/controllers.js
@@ -1,27 +1,8 @@
 /*global angular:true */
 angular.module('allure.core.controllers', [])
-    .controller('OverviewCtrl', function($scope, $storage, orderByFilter, status, percents, overview, defects, testsuites, widgets) {
+    .controller('OverviewCtrl', function($scope, $storage, widgets) {
         "use strict";
-        $scope.overview = overview;
-        $scope.defects = defects.defectsList.filter(function(defect) {
-            return defect.defects && defect.defects.length > 0;
-        });
-        $scope.defects.forEach(function(defect) {
-            defect.defects = orderByFilter(defect.defects, function(defect) {
-                return defect.testCases.length;
-            }, true);
-        });
-        $scope.statistic = testsuites.testSuites.reduce(function(statistic, testsuite) {
-            ['passed', 'pending', 'canceled', 'broken', 'failed', 'total'].forEach(function(status) {
-                statistic[status] += testsuite.statistic[status];
-            });
-            return statistic;
-        }, {
-            passed: 0, pending: 0, canceled: 0, failed: 0, broken: 0, total: 0
-        });
 
-        $scope.percents = percents($scope.statistic);
-        
         var store = $storage('allure-widgets'),
             storedWidgets = store.getItem('widgets') || widgets.reduce(function(all, widget, index) {
             all[index % 2].push(widget.name);
