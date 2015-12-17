a4824d63356dacd171bc283bbbaad3226a22b57d
just-boris <just-boris@yandex-team.ru>
2015-12-17 20:31:43 +0300

    new face

--- allure-report-face/src/test/webapp/vendor/ui-router-mock.js
+++
@@ -1,6 +0,0 @@
-angular.module('ui.router', []).provider('$state', function() {
-    "use strict";
-    var $stateProvider = jasmine.createSpyObj('stateProvider', ['state', '$get']);
-    $stateProvider.state.andReturn($stateProvider);
-    return $stateProvider;
-});
