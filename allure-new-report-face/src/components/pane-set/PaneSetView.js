8caa7c67a87bd9ec790e9f28faaee4f61fc97641
just-boris <just-boris@yandex-team.ru>
2015-12-22 19:06:05 +0300

    resizable panes

--- allure-new-report-face/src/components/pane-set/PaneSetView.js
+++ allure-new-report-face/src/components/pane-set/PaneSetView.js
@@ -8,18 +8,39 @@ const paneTpl = `<div class="pane"></div>`;
 @className('pane-set')
 class PaneSetView extends LayoutView {
     panes = [];
-    template() { return ''; }
+
+    template() {
+        return '';
+    }
 
     pushPane(view) {
         const pane = $(paneTpl);
         this.$el.append(pane);
         this.addRegion(this.panes.length, {el: pane}).show(view);
         this.panes.push(pane);
+        this.updatePanesPositions();
     }
 
     popPane() {
         this.removeRegion(this.panes.length - 1);
         this.panes.pop().remove();
+        this.updatePanesPositions();
+    }
+
+    updatePanesPositions() {
+        const last = this.panes.length - 1;
+        this.panes.forEach((pane, index) => {
+            var width, left;
+            if(index == last) {
+                width = index === 0 ? 100 : 50;
+                left = 100 - width;
+            } else {
+                const leftOffset = 5 * index;
+                left = leftOffset;
+                width = 50 - leftOffset;
+            }
+            pane.css({left: left + '%', width: width + '%'});
+        });
     }
 }
 
