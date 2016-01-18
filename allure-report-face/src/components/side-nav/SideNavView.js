6c8837e1ea4712cc6920a1e202c243293e34af2d
just-boris <just-boris@yandex-team.ru>
2016-01-18 16:28:16 +0300

    fetch report meta info add styles for links

--- allure-report-face/src/components/side-nav/SideNavView.js
+++ allure-report-face/src/components/side-nav/SideNavView.js
@@ -2,21 +2,31 @@ import './styles.css';
 import {className} from '../../decorators';
 import {ItemView} from 'backbone.marionette';
 import {Model} from 'backbone';
+import ReportModel from '../../data/report/ReportModel';
 import allurePlugins from '../../pluginApi';
 import template from './SideNavView.hbs';
 import router from '../../router';
 
 @className('side-nav')
 class SideNavView extends ItemView {
+    static reportModel = new ReportModel();
+
     template = template;
 
     initialize() {
-        this.model = new Model({
-            tabs: allurePlugins.tabs.map(({tabName, icon, title}) => ({
-                tabName, icon, title,
-                active: this.isTabActive(tabName)
-            }))
-        });
+        this.model = this.constructor.reportModel;
+        this.tabs = allurePlugins.tabs.map(({tabName, icon, title}) => ({
+            tabName, icon, title,
+            active: this.isTabActive(tabName)
+        }));
+        this.model.fetch().then(() => this.render())
+    }
+
+    serializeData() {
+        return {
+            tabs: this.tabs,
+            report: this.model.toJSON()
+        }
     }
 
     isTabActive(name) {
--- allure-report-face/src/helpers/linky.js
+++ allure-report-face/src/helpers/linky.js
@@ -3,5 +3,5 @@ import {SafeString} from 'handlebars/runtime';
 const URL_REGEXP = /^(\w)+:\/\/.*/;
 
 export default function(text) {
-    return URL_REGEXP.test(text) ? new SafeString(`<a href="${text}"  class="link link_theme_islands" target="_blank">${text}</a>`) : text;
+    return URL_REGEXP.test(text) ? new SafeString(`<a href="${text}"  class="link" target="_blank">${text}</a>`) : text;
 }
