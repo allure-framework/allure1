663d204b98c49c2c205c1da400f2e8abc625de47
just-boris <just-boris@yandex-team.ru>
2016-01-19 20:04:14 +0300

    tests on status toggle add code coverage (not-tested files are not shown in report)

--- allure-report-face/test/bootstrap.js
+++ allure-report-face/test/bootstrap.js
@@ -7,6 +7,11 @@ module.constructor._initPaths();
 const jsdom = require('jsdom').jsdom;
 const Handlebars = require('handlebars');
 require.cache[require.resolve('handlebars/runtime')] = {exports: Handlebars};
+Handlebars.registerHelper('helperMissing', function() {
+    const options = Array.from(arguments).pop();
+    const helper = require('helpers/' + options.name);
+    return helper.apply(this, arguments);
+});
 
 //jasmine addons
 global.joc = jasmine.objectContaining;
@@ -20,9 +25,10 @@ global.document = jsdom('<html><head></head><body></body></html>', {
     url: 'http://localhost'
 });
 global.window = global.document.defaultView;
+global.window.localStorage = jasmine.createSpyObj('localStorage', ['getItem', 'setItem']);
 global.navigator = global.window.navigator;
 global.location = global.window.location;
 
 //require hooks
-require.extensions['.css'] = function () {};
+require.extensions['.css'] = function() {};
 require('babel-core/register');
