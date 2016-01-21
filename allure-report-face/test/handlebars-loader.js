b97a768e3b79896d34cd2f32f014410768fd1283
just-boris <just-boris@yandex-team.ru>
2016-01-21 13:21:22 +0300

    prettify code of the loader-shim

--- allure-report-face/test/handlebars-loader.js
+++ allure-report-face/test/handlebars-loader.js
@@ -2,24 +2,39 @@
 const fs = require('fs');
 const path = require('path');
 const Handlebars = require('handlebars/dist/cjs/handlebars');
+
+const config = {
+    helperDirs: [
+        path.resolve(__dirname, '../src/helpers'),
+        path.resolve(__dirname, '../src/blocks')
+    ]
+};
+
 require.cache[require.resolve('handlebars/runtime')] = {exports: Handlebars};
+
 Handlebars.registerHelper('helperMissing', function() {
     if(arguments.length === 1) {
         return;
     }
     const options = Array.from(arguments).pop();
-    const helper = require('helpers/' + options.name);
+    const helper = config.helperDirs.reduce(function(helper, dir) {
+        return helper || require(path.join(dir, options.name));
+    }, null);
     return helper.apply(this, arguments);
 });
-const resolvePartial = Handlebars.VM.resolvePartial;
 
-require.extensions['.hbs'] = function(module, filename) {
-    Handlebars.VM.resolvePartial = function(partial, content, options) {
+const resolvePartial = Handlebars.VM.resolvePartial;
+function patchResolvePartial(filename) {
+    return function(partial, content, options) {
         if(!partial) {
             partial = require(path.resolve(path.dirname(filename), options.name) + '.hbs');
         }
         return resolvePartial.call(this, partial, content, options);
     };
+}
+
+require.extensions['.hbs'] = function(module, filename) {
+    Handlebars.VM.resolvePartial = patchResolvePartial(filename);
     var templateString = fs.readFileSync(filename, 'utf8');
     module.exports = Handlebars.compile(templateString);
 };
