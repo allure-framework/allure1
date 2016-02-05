d7c1832ac950e65f760b19982070865195d57224
just-boris <just-boris@yandex-team.ru>
2016-02-05 15:06:26 +0300

    do not show millis for timestamps

--- allure-report-face/src/helpers/time.js
+++ allure-report-face/src/helpers/time.js
@@ -4,5 +4,5 @@ export default function (date) {
     if(!(date instanceof Date)) {
         date = new Date(date);
     }
-    return [date.getHours(), pad(date.getMinutes(), 2, '0'), pad(date.getSeconds(), 2, '0')].join(':') + '.' + date.getMilliseconds();
+    return [date.getHours(), pad(date.getMinutes(), 2, '0'), pad(date.getSeconds(), 2, '0')].join(':');
 }
