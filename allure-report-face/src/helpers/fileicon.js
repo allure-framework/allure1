670f0ee148531b74dd844ec38837a360e9bf9f7f
Stas Seliverstov <stasseliverstov@MacBook-Pro-Stas.local>
2017-02-14 12:42:15 +0300

    fix bug and icon for archives

--- allure-report-face/src/helpers/fileicon.js
+++ allure-report-face/src/helpers/fileicon.js
@@ -1,17 +1,5 @@
 import typeByMime from '../util/attachmentType';
 
 export default function(type) {
-    switch (typeByMime(type).type) {
-        case 'text':
-            return 'fa fa-file-text-o';
-        case 'image':
-        case 'svg':
-            return 'fa fa-file-image-o';
-        case 'code':
-            return 'fa fa-file-code-o';
-        case 'table':
-            return 'fa fa-table';
-        default:
-            return 'fa fa-file-o';
-    }
+    return typeByMime(type).icon;
 }
